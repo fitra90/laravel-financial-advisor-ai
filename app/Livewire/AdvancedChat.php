e55ee40cfe974468f9a754a2934609bcaffe0049
Fitra Fadilana <fitra90@gmail.com>
2025-12-22 19:16:53 +0700

    fix get contacts for mentions

--- app/Livewire/AdvancedChat.php
+++ app/Livewire/AdvancedChat.php
@@ -160,7 +160,7 @@ class AdvancedChat extends Component
 
         // From Hubspot contacts
         // $contacts = $user->contacts->limit(50)->get();
-        $contacts = $user->contacts->sortByDesc('created_at')->take(10)->get();
+        $contacts = $user->contacts->all();
         foreach ($contacts as $contact) {
             if ($contact->email) {
                 Mention::updateOrCreate(
